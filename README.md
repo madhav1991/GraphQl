bcd30a05d672130130021849c05ee56853ad0107
Madhav Peri <mperi@informatica.com>
2025-01-12 18:48:24 -0500

    Add apollo graphql server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# GraphQl
-An attempt by me to cover graphql basic on client side
+https://www.youtube.com/watch?v=5199E50O7SI
+
+* Query lanaguage, differs from Rest
+* As the application scales, the data layer becomes more complex with over fetching and under fetching 
+* Query basics
+    - Sent using query keyword 
+    - We can name the query preceding the query keyword
+
+
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// server setup
+ const server = new ApolloServer({
+  //typeDefs,
+  //resolvers,
+ })
+
+ const {url} = await startStandaloneServer(server, {liste: {port: 4000}});
+ console.log("Server ready at port", 4000);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
